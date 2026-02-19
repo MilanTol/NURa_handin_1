7aa4416d9329e5f902ab08de63f50ee443b4fb37
Milan Tol <milantol35@gmail.com>
2026-02-19 15:10:43 +0100

    .

---
+++ algorithms/integration/integration_methods.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+def trapezoid(ys: np.ndarray, dx):
+    """
+    calculates area for sample points ys. Need to be equally spaced: dx
+    """
+    return dx * 0.5*  (np.sum(ys[:-1]) + np.sum(ys[1:])) 
+    
+
+def simpson(ys: np.ndarray, dx):
+    S1 = trapezoid(ys, dx)
+
+    dx = 2 * dx
+    ys = ys[0:-1:2]
+    S0 = trapezoid(ys, dx)
+
+    return (4*S1 - S0)/3
+
+
+def romberg(f: callable, a: np.float64, b: np.float64, m: np.int32, N:int = 1):
+    """
+    computes the romberg integral of some function between endpoints a and b,
+    using 2**order + 1 samples.
+    """
+    r = np.ndarray((m))
+
+    h = (b-a)/N
+    xs = np.linspace(a, b, N + 1)
+    r[0] = trapezoid(f(xs), xs[1] - xs[0])
+    
+    N_p = 1
+    for i in range(m)[1:]: #ranges from 1 to m-1
+        r[i] = 0
+        Delta = h
+        h = h/2 #Delta must be 2*h so that you dont recompute points from previous iterations!
+        x = a + h
+        
+        for j in range(N_p):
+            r[i] += f(x)
+            x = x + Delta
+
+        r[i] = 0.5 * (r[i-1] + Delta*r[i]) 
+        N_p *= 2
+
+    N_p = 1
+    for i in range(m)[1:]:
+        N_p *= 4
+        for j in range(m - i):
+            r[j] = (N_p * r[j+1] - r[j]) / (N_p - 1)
+
+    return r[0], np.abs(r[0] - r[1])
+
+
