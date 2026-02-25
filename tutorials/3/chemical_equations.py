8cee3d18fee56f48d34ef0b63768b09a75bbc410
Milan Tol <milantol35@gmail.com>
2026-02-25 17:42:14 +0100

    .

--- tutorials/3/chemical_equations.py
+++ tutorials/3/chemical_equations.py
@@ -1,5 +1,6 @@
 import sys
 import copy 
+import time
 
 sys.path.append("/home/milan/Desktop/NURa")
 
@@ -15,6 +16,9 @@ mat = np.array([
     [0, 1, 0, -12, -0]
 ])
 
+N = 200
+mat = np.random.random(size=(N, N))
+
 # mat = np.array([
 #     [3, 8, 1, -12, -4],
 #     [0, 1, 0, -1, -0],
@@ -26,19 +30,40 @@ mat = np.array([
 mat = Matrix(mat)
 
 b= np.array([2, 0, 1, 0, 0])
+b = np.random.random(size=(N,))
 b = Matrix(b)
 
 x_inv = mat.inverse()@b
-print(x_inv)
 print(mat@x_inv)
 
 print("")
 
 x_LU = mat.solve(b)
-print(x_LU)
 print(mat@x_LU)
 
-print("")
+# print("")
 
-print(mat.LU_decomposition())
+# print(mat.LU_decomposition())
 
+
+def gauss_method():
+    return mat.inverse()@b
+
+def LU_method():
+    return mat.solve(b)
+
+
+time1 = time.time()
+
+for i in range(10):
+    gauss_method()
+
+time2 = time.time()
+
+for i in range(10):
+    LU_method()
+
+time3 = time.time()
+
+print("Gauss_method:", time2 - time1)
+print("LU_method:", time3 - time2)
