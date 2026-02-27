b1f91eaed509cd258b83a5f49467abb7d9a7d528
Milan Tol <milantol35@gmail.com>
2026-02-27 10:59:20 +0100

    .

--- tutorials/3/chemical_equations.py
+++ tutorials/3/chemical_equations.py
@@ -26,6 +26,13 @@ mat = Matrix(mat)
 
 b = np.random.random(size=(N,))
 b= np.array([2, 0, 1, 0, 0])
+b = np.array([
+    [3, 8, 1, -12, -4],
+    [1, 0, 0, -1, -0],
+    [4, 4, 3, -40, -3],
+    [0, 2, 1, -3, -2],
+    [0, 1, 0, -12, -0]
+])
 b = Matrix(b)
 
 x_inv = mat.inverse()@b
